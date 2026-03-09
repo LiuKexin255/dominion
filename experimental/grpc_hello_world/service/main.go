9cdb221403d8c01ec01c2f212339230e4e088e4d
Liu Kexin <email@liukexin.com>
2026-03-09 06:18:06 +0000

    为 grpc hello world 增加 tls 配置与推送 image 命令

--- experimental/grpc_hello_world/service/main.go
+++ experimental/grpc_hello_world/service/main.go
@@ -9,10 +9,13 @@ import (
 	"dominion/experimental/grpc_hello_world"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 )
 
 var port = flag.String("port", "50051", "Port to listen on")
+var tlsCertFile = flag.String("tls_cert_file", "/etc/tls/tls.crt", "Path to TLS certificate file")
+var tlsKeyFile = flag.String("tls_key_file", "/etc/tls/tls.key", "Path to TLS private key file")
 
 type greeterServer struct {
 	grpc_hello_world.UnimplementedGreeterServer
@@ -37,11 +40,16 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	creds, err := credentials.NewServerTLSFromFile(*tlsCertFile, *tlsKeyFile)
+	if err != nil {
+		log.Fatalf("failed to load TLS key pair from cert=%s key=%s: %v", *tlsCertFile, *tlsKeyFile, err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 	grpc_hello_world.RegisterGreeterServer(grpcServer, &greeterServer{})
 	reflection.Register(grpcServer)
 
-	log.Printf("gRPC hello world server listening on :%s", *port)
+	log.Printf("gRPC hello world server listening with TLS on :%s", *port)
 	if err := grpcServer.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
